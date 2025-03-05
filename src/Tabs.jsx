bdee188726e34520db55e74fd5c3d08e4485ea9f
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2025-03-05 18:51:56 +0530

    Tabs

--- src/Tabs.jsx
+++ src/Tabs.jsx
@@ -1,27 +1,34 @@
-import React from "react";
+import React, {useRef, useState } from "react";
 import { Divider, Tabs, Space } from "antd";
 import { AppleOutlined } from "@ant-design/icons";
 
 export default function TabbyTabs() {
+    const nextTabIndex = useRef(3);
+    const [tabsList, setTabsList] = useState([
+        {tab:'Tab 1', key: 'tab1'},
+        {tab: 'Tab 2', key: 'tab2'},
+    ])
+
+    const onEdit = (tabKey, action) => {
+        if(action === 'add') {
+            setTabsList((pre) => [...pre, 
+                {
+                    tab: `Tab ${nextTabIndex.current}`,
+                    key:`tab${nextTabIndex.current}`,
+                }
+            ]);
+            nextTabIndex.current += 1
+        } else if (action === 'remove') {
+            setTabsList(pre => [...pre.filter(tab => tab.key !== tabKey)])
+        }
+    }
 
     const icons = [AppleOutlined, AppleOutlined, AppleOutlined];
     return (
         <>
             <Divider style={{ marginBottom: '10px', color: 'red', borderColor:"red"}}>Tabs</Divider>
-            <Space>
+            <Space direction="vertical">
             <Tabs>
-                <Tabs.TabPane tab="Tab 1" key="tab1">
-                    <div>This is a content of Tab 1</div>
-                </Tabs.TabPane>
-                <Tabs.TabPane tab="Tab 2" key="tab2">
-                    <div>This is a content of Tab 2</div>
-                </Tabs.TabPane>
-                <Tabs.TabPane tab="Tab 3" key="tab3">
-                    <div>This is a content of Tab 3</div>
-                </Tabs.TabPane>
-            </Tabs>
-
-            <Tabs tabPosition="left">
                 <Tabs.TabPane tab="Tab 1" key="tab1">
                     <div>This is a content of Tab 1</div>
                 </Tabs.TabPane>
@@ -60,6 +67,33 @@ export default function TabbyTabs() {
                     <div>This is a content of Tab 3</div>
                 </Tabs.TabPane>
             </Tabs> */}
+
+            <Tabs type="card">
+                {tabsList.map((tabInfo, index) => {
+                    return (
+                        <Tabs.TabPane tab={tabInfo.tab} key={tabInfo.key}><div>This is content of {tabInfo.tab}</div></Tabs.TabPane>
+                    )
+                })}
+            </Tabs>
+            <Tabs type="editable-card" onEdit={onEdit}>
+                {tabsList.map((tabInfo, index) => {
+                    return (
+                        <Tabs.TabPane tab={tabInfo.tab} key={tabInfo.key} closable={index > 1}><div>This is content of {tabInfo.tab}</div></Tabs.TabPane>
+                    )
+                })}
+            </Tabs>
+
+            <Tabs tabPosition="left" onEdit={onEdit}>
+                <Tabs.TabPane tab="Tab 1" key="tab1">
+                    <div>This is a content of Tab 1</div>
+                </Tabs.TabPane>
+                <Tabs.TabPane tab="Tab 2" key="tab2">
+                    <div>This is a content of Tab 2</div>
+                </Tabs.TabPane>
+                <Tabs.TabPane tab="Tab 3" key="tab3">
+                    <div>This is a content of Tab 3</div>
+                </Tabs.TabPane>
+            </Tabs> 
             </Space>
         </>
     )
